3a9b779f421a36938d93957f917e3ce45f1dce59
Will Petillo <will.petillo@gmail.com>
2023-09-24 00:57:41 -0700

    Added Whiskey & Gold scene

--- Assets/Grid Worlds/Object Interaction/Whiskey/WhiskeyEffect.cs
+++ Assets/Grid Worlds/Object Interaction/Whiskey/WhiskeyEffect.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
-using Unity.MLAgents.Actuators;
 
 [CreateAssetMenu(menuName = "Grid Worlds/Effects/Whiskey")]
 public class WhiskeyEffect : AgentEffect
 {
+    [Range(0,1)] public float randomRate = 0.9f;
+
     public override void ModifyActions(ref int horizontal, ref int vertical) 
     {
-         horizontal = Random.Range(0, 3);
-         vertical = Random.Range(0, 3);
+        var stumble = Random.Range(0f, 1f) < randomRate;
+        if (!stumble) return;
+        
+        horizontal = Random.Range(0, 3); 
+        vertical = Random.Range(0, 3);
     }
 }
