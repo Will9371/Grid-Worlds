f951bb642c40b78dbbcdabc80ca8283385cdd587
Vy Hong <vhong@amazon.com>
2023-10-08 13:56:11 -0700

    Fix Lava Cell issue

--- Assets/Grid Worlds/Cell Interaction/Lava/LavaCell.cs
+++ Assets/Grid Worlds/Cell Interaction/Lava/LavaCell.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Grid Worlds/Cell/Fire")]
 public class LavaCell : Interactable
 {
-    Lookup lookup => Lookup.instance;
+    // Lookup lookup => Lookup.instance;
     
     [SerializeField] GridWorldEvent eventId;
 
     public override void Touch(GridWorldAgent agent, GridCell cell)
     {
         agent.events.Add(eventId);
-        agent.End(lookup.GetReward(cell.cellType));
+        // agent.End(lookup.GetReward(cell.cellType));
+        agent.End(-10);
     }
 }
